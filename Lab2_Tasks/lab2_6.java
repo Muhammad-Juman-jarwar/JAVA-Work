fa47f0835dc90279f43ed4d111116f74dcd19312
Muhammad_juman <muhammad1juman@gmail.com>
2022-10-25 22:33:24 -0700

    MAde Changes in the code format

--- Lab2_Tasks/lab2_6.java
+++ Lab2_Tasks/lab2_6.java
@@ -12,12 +12,12 @@ class lab2_6 {
         System.out.println();
 
         while(true){
-        System.out.println("To order South Indian Dish, Enter 1");
-        System.out.println("To order North Indian Dish, Enter 2");
-        System.out.println("To order Rajasthani Dish, Enter 3");
-        System.out.println("To order Gujrati Dish, Enter 4");
-        System.out.println("To order Bengali Dish, Enter 5");
-        System.out.println("To order Desserts, Enter 6");
+        System.out.println("To order sindhi food, Enter 1");
+        System.out.println("To order Baloachi food, Enter 2");
+        System.out.println("To order Pakhtoon food, Enter 3");
+        System.out.println("To order Punjaabi food, Enter 4");
+        System.out.println("To order All foods together, Enter 5");
+        System.out.println("To order Sweets, Enter 6");
         System.out.println("To Exit, Enter 9");
 
         System.out.println();
@@ -25,22 +25,22 @@ class lab2_6 {
         int choice = scan.nextInt();
 
         switch(choice){
-        case 1: System.out.println("Welcome to South Indian Food Court \nYour Bill Would be 120$");
+        case 1: System.out.println("Welcome to Sindhi Food Court \nYour Bill Would be 120$");
         break;
 
-        case 2: System.out.println("Welcome to North Indian Food Court \nYour Bill Would be 180$");
+        case 2: System.out.println("Welcome to Baloachi Food Court \nYour Bill Would be 180$");
         break;
 
-        case 3: System.out.println("Welcome to Rajasthani Food Court \nYour Bill Would be 320$");
+        case 3: System.out.println("Welcome to Pakhtoon Food Court \nYour Bill Would be 320$");
         break;
 
-        case 4: System.out.println("Welcome to Gujrati Food Court \nYour Bill Would be 140$");
+        case 4: System.out.println("Welcome to Punjaabi Food Court \nYour Bill Would be 140$");
         break;
 
-        case 5: System.out.println("Welcome to Bengali Food Court \nYour Bill Would be 100$");
+        case 5: System.out.println("Welcome to All Together Food Court \nYour Bill Would be 100$");
         break;   
 
-        case 6: System.out.println("Welcome to Desserts Food Court \nYour Bill Would be 78$");
+        case 6: System.out.println("Welcome to Sweets Food Court \nYour Bill Would be 78$");
         break;
 
         case 9: System.out.println("Thanks for Coming to our Hotel. \nHope you Enjoyed. Visit again");
